61ae30be0469e27311ba2c8cf2f275d4103e24aa
rjmanzo <manzorenato@gmail.com>
2022-07-25 14:51:59 -0300

    Restoring to the previous point of app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,20 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
-import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [
-    CoffeesModule,
-    TypeOrmModule.forRoot({
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'secret123',
-      database: 'library',
-      synchronize: true,
-    }),
-  ],
+  imports: [CoffeesModule],
   controllers: [AppController],
   providers: [AppService],
 })
