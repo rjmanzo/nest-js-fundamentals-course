b10b3a1bf4796e4c1e820a5eba71fb46948ded9d
rjmanzo <manzorenato@gmail.com>
2022-07-25 14:51:05 -0300

    adding config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    TypeOrmModule.forRoot({
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'secret123',
+      database: 'library',
+      synchronize: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
